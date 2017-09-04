49892a6c3d47194a4c5946edc9f6c4edb0b18207
Eduard <eazymov@mail.ru>
2017-09-04 23:31:31 +0500

    added rating component

--- src/components/place/placeReviews/placeReview/index.tsx
+++ src/components/place/placeReviews/placeReview/index.tsx
@@ -6,40 +6,83 @@ type Review = App.Review;
 interface Props {
   review: Review;
 }
+
+interface State {
+  showFullText: boolean;
+}
 /* *** */
 
-import React from 'react';
+import React, { Component } from 'react';
 
 import Rating from './rating';
 
-const PlaceReview = (props: Props): JSX.Element => {
-  const review: Review = props.review;
-  const authorName: string = review.author_name;
-  const authorUrl: string = review.author_url;
-  // const language: string = review.language;
-  const rating: number = review.rating || 0;
-  const timeAgo: string = review.relative_time_description || '';
-  const profilePhotoUrl: string = review.profile_photo_url || '';
-  const text: string = review.text;
+class PlaceReview extends Component<Props, State> {
+  state = {
+    showFullText: false
+  };
 
-  return (
-    <li className="review">
-      <a className="review__avatar" href={authorUrl}>
-        <img src={profilePhotoUrl} alt={authorName} />
-      </a>
-      <div className="review__content">
-        <a
-          className="review__author"
-          href={authorUrl}
-        >
-          {authorName}
+  constructor(props: Props) {
+    super(props);
+  }
+
+  public render(): JSX.Element {
+    const review: Review = this.props.review;
+    const authorName: string = review.author_name;
+    const authorUrl: string = review.author_url;
+    const language: string = review.language;
+    const rating: number = review.rating || 0;
+    const timeAgo: string = review.relative_time_description || '';
+    const profilePhotoUrl: string = review.profile_photo_url || '';
+    const text: string = review.text;
+    const needTrim: boolean = text.length > 110;
+  
+    return (
+      <li className="review">
+        <a className="review__avatar" href={authorUrl}>
+          <img src={profilePhotoUrl} alt={authorName} />
         </a>
-        <Rating rating={rating} />
-        <span className="review__timeAgo">{timeAgo}</span>
-        <p className="review__text">{text}</p>
-      </div>
-    </li>
-  );
+        <div className="review__content">
+          <a
+            className="review__author"
+            href={authorUrl}
+          >
+            {authorName}
+          </a>
+          <Rating rating={rating} disabled={true} />
+          <span className="review__timeAgo">{timeAgo}</span>
+          <p lang={language} className="review__text">
+            {this.formatText(text, needTrim)}
+            {needTrim && this.toggleButton}
+          </p>
+        </div>
+      </li>
+    );
+  }
+
+  private get toggleButton(): JSX.Element {
+    const showFullText: boolean = this.state.showFullText;
+
+    return (
+      <span
+        className="show-more-btn"
+        onClick={() => this.setState({
+          showFullText: !showFullText
+        })}
+      >
+        {showFullText ? 'Hide' : 'Show'}
+      </span>
+    );
+  }
+
+  private formatText(text: string, needTrim: boolean): string {
+    const showFullText: boolean = this.state.showFullText;
+
+    if (showFullText || !needTrim) {
+      return text;
+    }
+
+    return text.slice(0, 107) + '...';
+  }
 }
 
 export default PlaceReview;
--- src/components/place/placeReviews/placeReview/rating/index.tsx
+++ src/components/place/placeReviews/placeReview/rating/index.tsx
@@ -3,26 +3,51 @@
  */
 interface Props {
   rating: number;
+  disabled: boolean;
+  size?: number;
+}
+
+interface State {
+  value: number;
 }
 /* *** */
 
-import React from 'react';
+import React, { Component } from 'react';
 
-const Rating = (props: Props): JSX.Element => {
-  return (
-    <div className="rating">
-      <input type="radio" hidden={true} />
-      <span className="star">&#9733;</span>
-      <input type="radio" hidden={true} />
-      <span className="star">&#9733;</span>
-      <input type="radio" hidden={true} />
-      <span className="star">&#9733;</span>
-      <input type="radio" hidden={true} />
-      <span className="star">&#9733;</span>
-      <input type="radio" hidden={true} />
-      <span className="star">&#9733;</span>
-    </div>
-  );
-};
+class Rating extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      value: props.rating || 0
+    };
+  }
+
+  public render(): JSX.Element {
+    const disabled: boolean = this.props.disabled;
+    const pointerEvents: string = disabled ? 'none' : 'auto';
+    const value: number = this.state.value;
+  
+    return (
+      <div className="rating" style={{ pointerEvents }}>
+        {
+          [5, 4, 3, 2, 1].map((key: number) => {
+            const className: string = (value === key) ? 'active' : '';
+            
+            return (
+              <span
+                key={key}
+                className={`star ${className}`}
+                onClick={() => this.setState({
+                  value: key
+                })}
+              />
+            );
+          })
+        }
+      </div>
+    );
+  }
+}
 
 export default Rating;
