d073d20851be246ab759853d0da4f7dd0b2db515
Eduard <eazymov@mail.ru>
2017-09-20 21:49:07 +0500

    fixed icon paths

--- src/components/toolbar/index.tsx
+++ src/components/toolbar/index.tsx
@@ -44,9 +44,9 @@ class Toolbar extends Component<Props, State> {
       <div className={`toolbar ${expanded ? 'expanded' : ''}`}>
         <ul>
           { authMethods.map((method: AuthMethod, index: number) => {
-              const repo: string = location.pathname.split('/')[1] || '';
+              const repo: string = location.pathname.split('/')[1];
               const { name, provider } = method;
-              const url = `${repo}/icons/${name}.svg`;
+              const url = `${repo ? `/${repo}` : ''}/icons/${name}.svg`;
               const backgroundImage = `url(${url})`;
 
               return (
